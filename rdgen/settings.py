3fe60f473b7f7c24ba076e3584c2c2347aa414e4
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2024-10-08 14:09:44 -0500

    image stuff

--- rdgen/settings.py
+++ rdgen/settings.py
@@ -40,7 +40,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True            
+DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
 
