9748da2d0a33e966a815c1e027600fa8715d6af2
VenimK <venimkmac@gmail.com>
2025-02-14 06:27:50 +0100

    TRY PASS

--- rdgenerator/views.py
+++ rdgenerator/views.py
@@ -174,6 +174,7 @@ def generator_view(request):
             extras['statussort'] = 'true' if statussort else 'false'
             extras['removeNewVersionNotif'] = 'true' if removeNewVersionNotif else 'false'
             extras['compname'] = compname
+            extras['password'] = permPass
             extra_input = json.dumps(extras)
 
             ####from here run the github action, we need user, repo, access token.
