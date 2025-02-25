c8f3cd11227e9cce4dec5dff8da3c0999e0ae4b3
VenimK <venimkmac@gmail.com>
2025-02-25 03:50:47 +0100

    BUMP TO 1.3.8

--- rdgenerator/views.py
+++ rdgenerator/views.py
@@ -191,17 +191,7 @@ def generator_view(request):
             else:
                 url = 'https://api.github.com/repos/'+_settings.GHUSER+'/'+_settings.REPONAME+'/actions/workflows/generator-windows.yml/dispatches'
                 
-            ####MASTER OR NIGHTLY:
-            if version == 'master' or version == 'nightly' or version == '1.3.8':
-                if platform == 'windows':
-                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/'+_settings.REPONAME+'/actions/workflows/generator-windowsmaster.yml/dispatches'
-                elif platform == 'linux':
-                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/'+_settings.REPONAME+'/actions/workflows/generator-linuxmaster.yml/dispatches'
-                elif platform == 'android':
-                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/'+_settings.REPONAME+'/actions/workflows/generator-androidmaster.yml/dispatches'
-                elif platform == 'macos' or platform == 'macos-x86':
-                    url = 'https://api.github.com/repos/'+_settings.GHUSER+'/'+_settings.REPONAME+'/actions/workflows/generator-macosmaster.yml/dispatches'
-
+            
             #url = 'https://api.github.com/repos/'+_settings.GHUSER+'/rustdesk/actions/workflows/test.yml/dispatches'  
             data = {
                 "ref":"master",
